9f0762fa189a3178b543aa432299620ab43fae8f
Jorge Luiz <jorgeluiz@MacBook-Pro-de-Mac.local>
2024-12-24 13:11:38 -0300

    projen

--- .projenrc.ts
+++ .projenrc.ts
@@ -9,7 +9,7 @@ const project = new CdktfProviderProject({
   releaseEveryCommit: true,
   minNodeVersion: "18.12.0",
   jsiiVersion: "~5.5.0",
-  minMajorVersion: 2,
+  minMajorVersion: 1,
   typescriptVersion: "~5.5.0", // NOTE: this should be the same major/minor version as JSII
   name: "cdktf-provider-grafana",
   packageName: "@jorgeLuizChaves/cdktf-provider-grafana",
